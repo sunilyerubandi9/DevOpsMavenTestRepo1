6ed1eadfbf7f0d0f825b0ed6093c6a3fe1fb4f47
Admin McAdmin <test@dummy.com>
2021-08-25 18:37:47 +0530

    Update Story

--- SunilMavenTestProject/src/main/java/com/snc/SunilMavenTestProject/App.java
+++ SunilMavenTestProject/src/main/java/com/snc/SunilMavenTestProject/App.java
@@ -6,6 +6,6 @@ package com.snc.SunilMavenTestProject;
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println("735: Story update 735");
+        System.out.println("736: Story update 736");
     }
 }
