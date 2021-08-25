f569a29cdc04abde7121e0e07a840c617c5410cd
Admin McAdmin <test@dummy.com>
2021-08-25 15:16:28 +0530

    Adding Maven Project

---
+++ TestProject_1.20/src/main/java/com/sn/TestProject_120/App.java
@@ -0,0 +1,13 @@
+package com.sn.TestProject_120;
+
+/**
+ * Hello world!
+ * new changes
+ */
+public class App {
+    public static void main( String[] args ) {
+        
+        System.out.println("742: Story update 742");
+   
+    }
+}
